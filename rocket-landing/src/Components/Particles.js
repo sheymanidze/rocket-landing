0bcab8fd6c36edd041b794f658be4659cbae8777
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2022-09-22 15:43:00 -0400

    background particles added

---
+++ rocket-landing/src/Components/Particles.js
@@ -0,0 +1,75 @@
+import Particles from "react-tsparticles"
+
+import { loadFull } from "tsparticles";
+
+const ParticlesBackground = () => {
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+
+  return <div>
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: '#162747',
+        },
+        fpsLimit: 40,
+        interactivity: {
+          detectsOn: 'canvas',
+          events: {
+            resize: true
+          },
+        },
+        particles: {
+          color: {
+            value: "#f1f1f1"
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1080
+            },
+            limit: 0,
+            value: 500,
+          },
+          opacity: {
+            animation: {
+              enable: true,
+              minimumValue: 0.5,
+              speed: 1,
+              sync: false,
+            },
+            random: {
+              enable: true,
+              minimumValue: 0.1,
+            },
+            value: 1,
+          },
+          shape: {
+            type: 'circle',
+
+          },
+          size: {
+            random: {
+              enable: true,
+              minimumValue: 0.5
+            },
+            value: 1
+          }
+        }
+      }}
+
+    />
+  </div >
+}
+
+export default ParticlesBackground;
---
+++ rocket-landing/src/Components/Rocket.js
@@ -0,0 +1,8 @@
+function Rocket() {
+  return (
+
+    <div>Hello</div>
+  );
+}
+
+export default Rocket;
